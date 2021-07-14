f33f8968380d0975bbd6dabaad5f0fce05b6d734
Julia Johnson <julialj95@gmail.com>
2021-07-13 22:11:36 -0500

    framed work with me section

---
+++ src/workWithMe.js
@@ -0,0 +1,13 @@
+function workWithMe() {
+  return (
+    <div>
+      <h2>Work with Me</h2>
+      <p>Contact information</p>
+      <form>
+        <label></label>
+        <input></input>
+      </form>
+    </div>
+  );
+}
+export default workWithMe;
